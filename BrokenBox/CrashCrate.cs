afde5daa0fb96e35e59706976632a36ef13b997b
skdydtjd <skghdrkq@naver.com>
2025-09-18 01:34:29 +0900

    Update from External Asset

--- BrokenBox/CrashCrate.cs
+++ BrokenBox/CrashCrate.cs
@@ -7,6 +7,7 @@
         [Header("Whole Create")]
         public MeshRenderer wholeCrate;
         public BoxCollider boxCollider;
+        
         [Header("Fractured Create")]
         public GameObject fracturedCrate;
 
@@ -14,6 +15,7 @@
 
         private void OnTriggerEnter(Collider other)
         {
+            // 아무데나 부서지지 않도록 충돌 조건 수정
             if(other.gameObject.tag == "prison" || other.gameObject.tag == "Player"
                 || other.gameObject.tag == "outside" || other.gameObject.tag == "Untagged" 
                 || other.gameObject.tag == "Enemy" || other.gameObject.tag == "WoodBox")
@@ -24,6 +26,8 @@
             wholeCrate.enabled = false;
             boxCollider.enabled = false;
             fracturedCrate.SetActive(true);
+
+            // 기존 소리출력에서 효과음 관리에 의해 출력되도록 수정
             if (!playsound)
             {
                 SFXMusic.Instance.Play("BoxCrash");
