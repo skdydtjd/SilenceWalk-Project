4410fa8f668d1b06c21a8b596136c4a5335b40ab
skdydtjd <skghdrkq@naver.com>
2026-05-24 04:31:06 +0900

    Update UI Quality and Anim with DOTween

--- Puzzle2/Puzzle2KeyUI.cs
+++ Puzzle2/Puzzle2KeyUI.cs
@@ -1,34 +1,96 @@
 using UnityEngine;
+using DG.Tweening;
 
 public class Puzzle2KeyUI : MonoBehaviour
 {
     public GameObject puzzle2Keypenal;
 
+    [SerializeField] private float fadeInDuration = 1.5f;
+    [SerializeField] private float fadeOutDuration = 1f;
+
+    private CanvasGroup puzzle2CanvasGroup;
+    private Tween fadeTween;
+
     private void OnTriggerEnter(Collider other)
     {
-        if(other.gameObject.tag == "Player")
+        if(other.CompareTag("Player"))
         {
-            puzzle2Keypenal.SetActive(true);
+            ShowPuzzle2Key();
         }
     }
 
     private void OnTriggerExit(Collider other)
     {
-        if (other.gameObject.tag == "Player")
+        if (other.CompareTag("Player"))
         {
-            puzzle2Keypenal.SetActive(false);
+            HidePuzzle2Key();
         }
     }
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        if (!PreparePuzzle2KeyPanel())
+        {
+            return;
+        }
+
+        puzzle2CanvasGroup.alpha = 0f;
+        puzzle2Keypenal.SetActive(false);
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnDestroy()
     {
-        
+        fadeTween?.Kill();
+    }
+
+    private void ShowPuzzle2Key()
+    {
+        if (!PreparePuzzle2KeyPanel())
+        {
+            return;
+        }
+
+        fadeTween?.Kill();
+        puzzle2Keypenal.SetActive(true);
+        fadeTween = puzzle2CanvasGroup.DOFade(1f, fadeInDuration);
+    }
+
+    private void HidePuzzle2Key()
+    {
+        if (!PreparePuzzle2KeyPanel())
+        {
+            return;
+        }
+
+        fadeTween?.Kill();
+        fadeTween = puzzle2CanvasGroup.DOFade(0f, fadeOutDuration)
+            .OnComplete(() =>
+            {
+                if (puzzle2Keypenal != null)
+                {
+                    puzzle2Keypenal.SetActive(false);
+                }
+            });
+    }
+
+    private bool PreparePuzzle2KeyPanel()
+    {
+        if (puzzle2Keypenal == null)
+        {
+            return false;
+        }
+
+        if (puzzle2CanvasGroup == null)
+        {
+            puzzle2CanvasGroup = puzzle2Keypenal.GetComponent<CanvasGroup>();
+
+            if (puzzle2CanvasGroup == null)
+            {
+                puzzle2CanvasGroup = puzzle2Keypenal.AddComponent<CanvasGroup>();
+            }
+        }
+
+        return true;
     }
 }
--- StartKeyUI.cs
+++ StartKeyUI.cs
@@ -1,30 +1,92 @@
 using UnityEngine;
+using DG.Tweening;
 
 public class StartKeyUI : MonoBehaviour
 {
     public GameObject keypenal;
 
+    [SerializeField] private float fadeInDuration = 1.5f;
+    [SerializeField] private float fadeOutDuration = 1f;
+    [SerializeField] private float visibleDuration = 10f;
+
+    private CanvasGroup keyCanvasGroup;
+    private Tween fadeTween;
+
     public void showpenal()
     {
+        if (!PrepareKeyPanel())
+        {
+            return;
+        }
+
+        fadeTween?.Kill();
         keypenal.SetActive(true);
 
-        Invoke("hidepenal", 10f);
+        fadeTween = DOTween.Sequence()
+            .Append(keyCanvasGroup.DOFade(1f, fadeInDuration))
+            .AppendInterval(visibleDuration)
+            .Append(keyCanvasGroup.DOFade(0f, fadeOutDuration))
+            .OnComplete(() =>
+            {
+                if (keypenal != null)
+                {
+                    keypenal.SetActive(false);
+                }
+            });
     }
 
     public void hidepenal()
     {
-        keypenal.SetActive(false);
+        if (!PrepareKeyPanel())
+        {
+            return;
+        }
+
+        fadeTween?.Kill();
+        fadeTween = keyCanvasGroup.DOFade(0f, fadeOutDuration)
+            .OnComplete(() =>
+            {
+                if (keypenal != null)
+                {
+                    keypenal.SetActive(false);
+                }
+            });
     }
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        if (!PrepareKeyPanel())
+        {
+            return;
+        }
+
+        keyCanvasGroup.alpha = 0f;
+        keypenal.SetActive(false);
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnDestroy()
     {
-        
+        fadeTween?.Kill();
+    }
+
+    private bool PrepareKeyPanel()
+    {
+        if (keypenal == null)
+        {
+            return false;
+        }
+
+        if (keyCanvasGroup == null)
+        {
+            keyCanvasGroup = keypenal.GetComponent<CanvasGroup>();
+
+            if (keyCanvasGroup == null)
+            {
+                keyCanvasGroup = keypenal.AddComponent<CanvasGroup>();
+            }
+        }
+
+        return true;
     }
 }
